b147de386bb64bd972903ab8a246c91b1441c77c
bloshchinenko_sg <sergey.bloshchinenko@el5-energo.ru>
2025-02-13 15:37:26 +0300

    test primer

--- TestEremex/MyViewModel.cs
+++ TestEremex/MyViewModel.cs
@@ -4,6 +4,8 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
 using Eremex.AvaloniaUI.Charts;
 
 namespace TestEremex;
@@ -12,7 +14,23 @@ public class MyViewModel : INotifyPropertyChanged
 {
     public event PropertyChangedEventHandler? PropertyChanged;
     public SortedDateTimeDataAdapter DataAdapter { get; set; } = new();
-    //public SortedTimeSpanDataAdapter DataAdapter { get; set; } = new();
+
+    public MyViewModel()
+    {
+        Collection.Add("test");
+        Collection.Add("test1");
+        Collection.Add("test2");
+        Collection.Add("test3");
+        
+        
+        Collection2.Add(new ItemSelect( ){Id=1, Name="test"});
+        Collection2.Add(new ItemSelect( ){Id=1, Name="test1"});
+        Collection2.Add(new ItemSelect( ){Id=1, Name="test2"});
+        Collection2.Add(new ItemSelect( ){Id=1, Name="test3"});
+        Items.Add(new ItemData(){Id=1, Trigger = null});
+        Items.Add(new ItemData(){Id=2, Trigger = "3test2"});
+        Items.Add(new ItemData(){Id=3, Trigger = null});
+    }
 
     protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
@@ -28,4 +46,72 @@ public class MyViewModel : INotifyPropertyChanged
         return true;
     }
 
+    public ICommand ClearCommand { get; set; } = new RelayCommand(Test);
+
+    private static void Test()
+    {
+        
+    }
+
+    public void ClearTest()
+    {
+        foreach (ItemData itemData in Items)
+            itemData.Trigger = null;
+        View.GridControl.CloseEditor();
+    }
+    
+
+    public ObservableCollection<ItemData> Items { get; set; } = new();
+    public ObservableCollection<string> Collection { get; set; } = new();
+    public ObservableCollection<ItemSelect> Collection2 { get; set; } = new();
+    public MainWindow View { get; set; }
+
+    public void LoadData()
+    {
+        Collection.Clear();
+        Collection.Add("1test");
+        Collection.Add("2test1");
+        Collection.Add("3test2");
+        Collection.Add("4test3");
+        
+        Collection2.Clear();
+        Collection2.Add(new ItemSelect( ){Id=1, Name="1test"});
+        Collection2.Add(new ItemSelect( ){Id=1, Name="2test1"});
+        Collection2.Add(new ItemSelect( ){Id=1, Name="3test2"});
+        Collection2.Add(new ItemSelect( ){Id=1, Name="4test3"});
+        OnPropertyChanged(nameof(Collection2));
+        OnPropertyChanged(nameof(Collection));
+    }
+}
+
+public class ItemSelect
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class ItemData : INotifyPropertyChanged
+{
+    private string? _trigger;
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+
+    public int Id { get; set; }
+    public string? Trigger
+    {
+        get => _trigger;
+        set => SetField(ref _trigger, value);
+    }
 }
---
+++ TestEremex/emxLicense.cs
@@ -0,0 +1,16 @@
+﻿// This file is auto-generated. Please do not change it.
+using System.Runtime.CompilerServices;
+
+using Eremex.AvaloniaUI.Controls.License;
+
+namespace TestEremex;
+public class LicenseProvider
+{
+#pragma warning disable CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+	[ModuleInitializer]
+#pragma warning restore CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+    public static void RegisterLicense()
+	{
+        ControlsLicenseManager.SetRuntimeLicenseOwner(new LicenseProvider(),"", "EE FD BD 22 74 C7 60 DC E3 25 D9 8B AA 62 08 85 C0 34 83 95 89 69 51 9F 7F 04 07 A6 45 1C 54 7E 07 D0 81 A8 15 EF 8D FE");
+	}
+}
